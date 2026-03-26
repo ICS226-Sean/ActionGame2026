935b67dbfd10ec74e4ef625e6500f3143c558837
Sean <ICS25-030@camosun.ca>
2026-03-26 10:38:26 -0700

    Initial Commit

---
+++ ActionGame2026Tests/ActionGame2026Tests.swift
@@ -0,0 +1,19 @@
+//
+//  ActionGame2026Tests.swift
+//  ActionGame2026Tests
+//
+//  Created by  on 2026-03-26.
+//
+
+import Testing
+@testable import ActionGame2026
+
+struct ActionGame2026Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
