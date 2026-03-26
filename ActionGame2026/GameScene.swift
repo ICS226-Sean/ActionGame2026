0c0d2b35146b2b08d90fe320e9bbd81bd0e36a4b
Sean <ICS25-030@camosun.ca>
2026-03-26 11:22:03 -0700

    Step 2 - Wumpa

--- ActionGame2026/GameScene.swift
+++ ActionGame2026/GameScene.swift
@@ -6,6 +6,14 @@ class GameScene: SKScene {
         sprite.position = CGPoint(x: size.width / 2, y: size.height / 2)
         sprite.size = CGSize(width: 50, height: 50)
         addChild(sprite)
+        
+        let opponentSprite = SKSpriteNode(imageNamed: "OpponentSprite")
+        opponentSprite.position = CGPoint(x: size.width / 2, y: size.height)
+        addChild(opponentSprite)
+        let downMovement = SKAction.move(to: CGPoint(x: size.width / 2, y: 0), duration: 1)
+        let upMovement = SKAction.move(to: CGPoint(x: size.width / 2, y: size.height), duration: 1)
+        let movement = SKAction.sequence([downMovement, upMovement])
+        opponentSprite.run(SKAction.repeatForever(movement))
     }
     func touchDown(atPoint pos : CGPoint) {
         
