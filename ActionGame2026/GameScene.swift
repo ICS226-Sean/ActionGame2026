6b80a7277c4333835db5cf8aa542faf33b268069
Sean <ICS25-030@camosun.ca>
2026-03-26 12:01:42 -0700

    Part 3 - Added Score Label

--- ActionGame2026/GameScene.swift
+++ ActionGame2026/GameScene.swift
@@ -1,19 +1,57 @@
 import SpriteKit
-class GameScene: SKScene {
+import GameplayKit
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
     var sprite : SKSpriteNode!
+    var scoreLabel:SKLabelNode!
+    
+    let spriteCategory1 : UInt32 = 0b1
+    let spriteCategory2 : UInt32 = 0b10
+    let opponentSprite = SKSpriteNode(imageNamed: "OpponentSprite")
+    
+    var score: Int = 0 {
+        didSet {
+            scoreLabel.text = "Score : \(score) "
+        }
+    }
+    
     override func didMove(to view: SKView) {
+        
+        scoreLabel = SKLabelNode()
+        scoreLabel.text = "Score: \(score)"
+        scoreLabel.fontSize = 45
+        scoreLabel.fontColor = SKColor.white
+        scoreLabel.position = CGPoint(x: size.width / 2, y: size.height - 60)
+        scoreLabel.horizontalAlignmentMode = .center
+        scoreLabel.verticalAlignmentMode = .center
+        
+        self.addChild(scoreLabel)
+        
         sprite = SKSpriteNode(imageNamed: "PlayerSprite")
         sprite.position = CGPoint(x: size.width / 2, y: size.height / 2)
         sprite.size = CGSize(width: 50, height: 50)
         addChild(sprite)
         
-        let opponentSprite = SKSpriteNode(imageNamed: "OpponentSprite")
+        sprite.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        opponentSprite.physicsBody = SKPhysicsBody(circleOfRadius: 50)
+        sprite.physicsBody?.categoryBitMask = spriteCategory1
+        sprite.physicsBody?.contactTestBitMask = spriteCategory1
+        sprite.physicsBody?.collisionBitMask = spriteCategory1
+        opponentSprite.physicsBody?.categoryBitMask = spriteCategory1
+        opponentSprite.physicsBody?.contactTestBitMask = spriteCategory1
+        opponentSprite.physicsBody?.collisionBitMask = spriteCategory1
+        self.physicsWorld.contactDelegate = self
+        
         opponentSprite.position = CGPoint(x: size.width / 2, y: size.height)
         addChild(opponentSprite)
         let downMovement = SKAction.move(to: CGPoint(x: size.width / 2, y: 0), duration: 1)
         let upMovement = SKAction.move(to: CGPoint(x: size.width / 2, y: size.height), duration: 1)
         let movement = SKAction.sequence([downMovement, upMovement])
-        opponentSprite.run(SKAction.repeatForever(movement))
+        moveOpponent()
+    }
+    func didBegin(_ contact: SKPhysicsContact) {
+        score += 1
+        print("Hit!")
     }
     func touchDown(atPoint pos : CGPoint) {
         
@@ -37,4 +75,13 @@ class GameScene: SKScene {
     }
     override func update(_ currentTime: TimeInterval) {
     }
+    func moveOpponent() {
+        let randomX = GKRandomSource.sharedRandom().nextInt(upperBound: Int(size.width))
+        let randomY = GKRandomSource.sharedRandom().nextInt(upperBound: Int(size.height))
+        let movement = SKAction.move(to: CGPoint(x: randomX, y: randomY), duration: 1)
+        opponentSprite.run(movement, completion: { [unowned self] in
+            self.moveOpponent()
+        })
+    }
+    
 }
